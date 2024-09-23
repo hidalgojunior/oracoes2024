dbfede65f807bdebfc33b0b4e06700991c46d402
Arnaldo Martins Hidalgo Junior <arnaldo@hidalgojunior.com.br>
2024-09-23 00:11:28 -0300

    Migration para criação da tabela de orações

---
+++ database/migrations/2024_09_23_030848_create_oracoes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('oracoes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_solicitante');
+            $table->text('pedido_oracao');
+            $table->string('status')->default('pendente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('oracoes');
+    }
+};
